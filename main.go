4f71e1899d6578c0e3228c4b671b91d3cab2aacf
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-12-07 22:45:49 +0100

    add fft

--- main.go
+++ main.go
@@ -19,14 +19,20 @@ import (
 	"encoding/gob"
 	"flag"
 	"image"
+	"image/color"
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
+	"sync"
 	"time"
 
 	"github.com/disintegration/imaging"
+	"github.com/mjibson/go-dsp/fft"
 	"github.com/pkg/errors"
 )
+
+const Width = 128
 
 func main() {
 	flagDB := flag.String("db", "mosaic.db", "DB file for thumbnails")
@@ -52,43 +58,80 @@ func Main(outFn, dbFn string, files []string) error {
 	if err != nil {
 		return err
 	}
-	sortedThumbs := sortThumbs(thumbnails)
+	sortedThumbs := newSortedThumbnails(thumbnails, files)
+	for i, t := range sortedThumbs {
+		log.Printf("%d. %s %#v=%v", i, t.Name, t.FFT[0], R(t.FFT[0]))
+	}
 
-	pattern, err := imaging.Open(files[0])
+	target, err := imaging.Open(files[0])
 	if err != nil {
 		return errors.Wrap(err, files[0])
 	}
 
-	mul := 3
-	b := pattern.Bounds()
-	W, H := b.Max.X-b.Min.X, b.Max.Y-b.Min.Y
-	for len(thumbnails)*mul < W || len(thumbnails)*mul < H {
-		mul++
+	n := 3
+	for n*n < len(files) {
+		n++
 	}
-	log.Println(mul)
-	pat := imaging.Resize(pattern, W/mul, H/mul, imaging.Lanczos)
+	log.Printf("Will use %d*%d=%d files", n, n, n*n)
 
-	b = pat.Bounds()
-	for i := b.Min.Y; i < b.Max.Y; i += 3 {
-		for j := b.Min.X; j < b.Max.X; j += 3 {
-			found := find(sortedThumbs, imaging.Crop(pat, image.Rectangle{Min: image.Point{X: b.Min.X + i, Y: b.Min.Y + j}, Max: image.Point{X: b.Min.X + i + 3, Y: b.Min.Y + j + 3}}))
+	//b := target.Bounds()
+	//W, H := b.Max.X-b.Min.X, b.Max.Y-b.Min.Y
+	tgt := imaging.Resize(target, n*Width, n*Width, imaging.Lanczos)
+	b := tgt.Bounds()
+	for i := b.Min.Y; i < b.Max.Y; i += Width {
+		for j := b.Min.X; j < b.Max.X; j += Width {
+			found := sortedThumbs.FindImg(
+				imaging.Crop(
+					tgt,
+					image.Rectangle{
+						Min: image.Point{X: b.Min.X + i, Y: b.Min.Y + j},
+						Max: image.Point{X: b.Min.X + i + Width, Y: b.Min.Y + j + Width},
+					},
+				),
+			)
+			log.Println(found)
 		}
 	}
 
 	return out.Close()
 }
 
-func thumbLess(a, b *image.NRGBA) bool {
-	for _, i := range []int{5,
-		2, 6, 8, 4,
-		1, 3, 7, 9,
-	} {
-		return a.Pix[(i-1)*4:i*4] < b.Pix[(i-1)*4:i*4]
+func R(c complex128) float64 { return real(c)*real(c) + imag(c)*imag(c) }
+
+func thumbLess(a, b [Width * Width]complex128) bool {
+	for i, va := range a {
+		vb := b[i]
+		if ra, rb := R(va), R(vb); ra > rb {
+			return false
+		}
 	}
+	return true
 }
-func sortThumbs(thumbnails map[string]Thumbnail) map[[9]uint8]string {
-	sorted := make(map[[9]uint8]string, len(thumbnails))
+
+type sortedThumbnails []Thumbnail
+
+func newSortedThumbnails(thumbnails map[string]Thumbnail, files []string) sortedThumbnails {
+	sorted := make([]Thumbnail, len(files))
+	for i, fn := range files {
+		sorted[i] = thumbnails[fn]
+	}
+	sort.Slice(
+		sorted,
+		func(i, j int) bool { return thumbLess(sorted[i].FFT, sorted[j].FFT) },
+	)
 	return sorted
+}
+
+func (s sortedThumbnails) FindImg(img image.Image) string {
+	needle := imgFFT(img)
+	log.Printf("needle[0]=%#v=%v", needle[0], R(needle[0]))
+	i := sort.Search(len(s), func(i int) bool { return thumbLess(needle, s[i].FFT) })
+	if i < 0 {
+		i = 0
+	} else if i >= len(s) {
+		i = len(s) - 1
+	}
+	return s[i].Name
 }
 
 func prepareThumbnails(dbFn string, files []string) (map[string]Thumbnail, error) {
@@ -96,12 +139,13 @@ func prepareThumbnails(dbFn string, files []string) (map[string]Thumbnail, error
 	if dbFh, err := os.Open(dbFn); err == nil {
 		gob.NewDecoder(dbFh).Decode(&thumbnails)
 	}
-	for _, fn := range files {
+	for i, fn := range files {
 		fn, err := filepath.Abs(fn)
 		if err != nil {
 			log.Println(errors.Wrap(err, fn))
 			continue
 		}
+		files[i] = fn
 		fi, err := os.Stat(fn)
 		if err != nil {
 			log.Println(errors.Wrap(err, fn))
@@ -116,10 +160,9 @@ func prepareThumbnails(dbFn string, files []string) (map[string]Thumbnail, error
 			log.Println(errors.Wrap(err, fn))
 			continue
 		}
-		thumb.NRGBA = imaging.Resize(img, 3, 3, imaging.Linear)
+		thumb.FFT = imgFFT(img)
 		thumbnails[fn] = thumb
 	}
-	log.Printf("%#v", thumbnails)
 
 	dbFh, err := os.Create(dbFn)
 	if err != nil {
@@ -135,5 +178,45 @@ func prepareThumbnails(dbFn string, files []string) (map[string]Thumbnail, error
 type Thumbnail struct {
 	Name    string
 	ModTime time.Time
-	*image.NRGBA
+	FFT     [Width * Width]complex128
+}
+
+type backing struct {
+	Array  [Width * Width]float64
+	Matrix [][]float64
+}
+
+var backingPool = sync.Pool{New: func() interface{} {
+	var b backing
+	b.Matrix = make([][]float64, Width)
+	for i := range b.Matrix {
+		b.Matrix[i] = b.Array[i*Width : (i+1)*Width]
+	}
+	return &b
+}}
+
+func imgFFT(img image.Image) [Width * Width]complex128 {
+	nrgba, _ := img.(*image.NRGBA)
+	if nrgba == nil || img.ColorModel() != color.GrayModel {
+		nrgba = imaging.Grayscale(img)
+	}
+	if b := nrgba.Bounds(); b.Max.X-b.Min.X > Width || b.Max.Y-b.Min.Y > Width {
+		nrgba = imaging.Resize(nrgba, Width, Width, imaging.Lanczos)
+	}
+
+	b := backingPool.Get().(*backing)
+	// TODO(tgulacsi): spiral from the center
+	for i := 0; i < Width; i++ {
+		for j := 0; j < Width; j++ {
+			b.Array[i*Width+j] = float64(nrgba.Pix[nrgba.PixOffset(i, j)])
+		}
+	}
+	mtx := fft.FFT2Real(b.Matrix)
+	var carr [Width * Width]complex128
+	for i, vv := range mtx {
+		for j, v := range vv {
+			carr[i*Width+j] = v
+		}
+	}
+	return carr
 }
